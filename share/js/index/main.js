6c546020aea7a3e2fb6cb6b0b84b4b62227c4939
vewe_richard <jiang.j.qian@qq.com>
2025-11-25 12:10:47 +0000

    change path of kvm to xxx

--- share/js/index/main.js
+++ share/js/index/main.js
@@ -95,7 +95,7 @@ function __showKvmdInfo(info) {
 		|| tools.config.getBool("index--hide-kvm-button", false)
 	);
 	if (!hide_kvm_button) {
-		html += __makeApp(null, "kvm", "share/svg/kvm.svg", "KVM");
+		html += __makeApp(null, "xxx", "share/svg/kvm.svg", "XXX");
 	}
 
 	for (let app of apps) {
